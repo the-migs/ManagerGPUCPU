747b6db4d21a48f198face517da7d1bb219a79bf
the-migs <migsgomeas@gmail.com>
2026-01-23 23:09:11 -0300

    Updates

--- src/All/CameraASCII.java
+++ src/All/CameraASCII.java
@@ -12,10 +12,21 @@ public class CameraASCII {
     static Font font = new Font("Monospaced", Font.PLAIN, 12);
     static Webcam webcam;
     static boolean running = true;
+    static Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+
     // frame compartilhado entre threads (visibilidade garantida)
     static volatile BufferedImage frame;
     static volatile BufferedImage grayImage;
     static volatile BufferedImage ASCIIImage;
+
+    static volatile String characterValue = "  .=10?XY";
+    static volatile String almostNoLightingString = " .=10!?XY";
+    static volatile String littleLightingString = "  .=10?LYX";
+    static volatile String considerableLightingString = "  .=10?XY";
+    static volatile String mediumLightingString = "  .=10?X";
+    static volatile String muchLightingString = "  .=10?";
+    static volatile String extremeLightingString = "   .=10?";
+
     static void metodSuprem() {
         System.out.println("metodSuprem");
 
@@ -43,40 +54,174 @@ public class CameraASCII {
     }
 }
 class Window extends JFrame {
+    static ImagePanel imagePanel;
+    static JSplitPane split;
+    private Timer repaintTimer;
     Image icon = Toolkit.getDefaultToolkit().getImage(Window.class.getResource("/A.png"));
     Window(){
         setTitle("Camera ASCII");
-        // setSize(1280, 720);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 CameraASCII.running = false;
+                if (repaintTimer != null) {
+                    repaintTimer.stop();
+                }
                 if (CameraASCII.webcam != null && CameraASCII.webcam.isOpen()) {
-                    CameraASCII.webcam.close(); // fecha a webcam
+                    CameraASCII.webcam.close();
                 }
             }
         });
+
         setIconImage(icon);
         setBackground(Color.BLACK);
-        ImagePanel image = new ImagePanel();
-        add(image);
+
+        imagePanel = new ImagePanel();
+        JPanel controls = new JPanel();
+        controls.setLayout(new BoxLayout(controls, BoxLayout.Y_AXIS));
+        controls.setBorder(BorderFactory.createLineBorder(Color.CYAN, 4));
+        JLabel title = new JLabel("> Lighting configuration <");
+        title.setForeground(Color.CYAN);
+
+        title.setFont(new Font("SansSerif", Font.BOLD, 60));
+        title.setBorder(BorderFactory.createLineBorder(Color.CYAN, 3));
+
+
+        JButton almostNoButton = new JButton("Almost No lighting");
+        almostNoButton.setFont(new Font("SansSerif", Font.BOLD, 30));
+        almostNoButton.setFocusPainted(false);
+        almostNoButton.setPreferredSize(new Dimension(455, 55)); // quadrado 120x120
+        almostNoButton.setMinimumSize(new Dimension(455, 55));
+        almostNoButton.setMaximumSize(new Dimension(455, 55));
+
+        JButton littleButton = new JButton("Little Lighting");
+        littleButton.setFont(new Font("SansSerif", Font.BOLD, 30));
+        littleButton.setFocusPainted(false);
+        littleButton.setPreferredSize(new Dimension(455, 55));
+        littleButton.setMinimumSize(new Dimension(455, 55));
+        littleButton.setMaximumSize(new Dimension(455, 55));
+
+        JButton considerableButton = new JButton("Considerable Lighting");
+        considerableButton.setFont(new Font("SansSerif", Font.BOLD, 30));
+        considerableButton.setFocusable(false);
+        considerableButton.setPreferredSize(new Dimension(455, 55));
+        considerableButton.setMinimumSize(new Dimension(455, 55));
+        considerableButton.setMaximumSize(new Dimension(455, 55));
+
+        JButton mediumButton = new JButton("Medium Lighting");
+        mediumButton.setFont(new Font("SansSerif", Font.BOLD, 30));
+        mediumButton.setFocusable(false);
+        mediumButton.setPreferredSize(new Dimension(455, 55));
+        mediumButton.setMinimumSize(new Dimension(455, 55));
+        mediumButton.setMaximumSize(new Dimension(455, 55));
+
+        JButton muchButton = new JButton("Much Lighting");
+        muchButton.setFont(new Font("SansSerif", Font.BOLD, 30));
+        muchButton.setFocusable(false);
+        muchButton.setPreferredSize(new Dimension(455, 55));
+        muchButton.setMinimumSize(new Dimension(455, 55));
+        muchButton.setMaximumSize(new Dimension(455, 55));
+
+        JButton extremeButton = new JButton("Extreme Lighting");
+        extremeButton.setFont(new Font("SansSerif", Font.BOLD, 30));
+        extremeButton.setFocusable(false);
+        extremeButton.setPreferredSize(new Dimension(455, 55));
+        extremeButton.setMinimumSize(new Dimension(455, 55));
+        extremeButton.setMaximumSize(new Dimension(455, 55));
+
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        almostNoButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        littleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        considerableButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mediumButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        muchButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        extremeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        almostNoButton.addActionListener(e -> {
+            CameraASCII.characterValue = CameraASCII.almostNoLightingString;
+        });
+
+        littleButton.addActionListener(e -> {
+            CameraASCII.characterValue = CameraASCII.littleLightingString;
+        });
+
+        considerableButton.addActionListener(e -> {
+            CameraASCII.characterValue = CameraASCII.considerableLightingString;
+        });
+
+        mediumButton.addActionListener(e -> {
+           CameraASCII.characterValue = CameraASCII.mediumLightingString;
+        });
+
+        muchButton.addActionListener(e -> {
+           CameraASCII.characterValue = CameraASCII.muchLightingString;
+        });
+
+        extremeButton.addActionListener(e -> {
+            CameraASCII.characterValue = CameraASCII.extremeLightingString;
+        });
+
+        split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,imagePanel,controls);
+
+        imagePanel.setBackground(Color.BLACK);
+
+        setLayout(new BorderLayout());
+        add(split, BorderLayout.CENTER);
+        split.setResizeWeight(0.5); // 50% / 50%
+        split.setDividerSize(0);    // sem barra visível
+        controls.setBackground(Color.WHITE);
+
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(title);
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(almostNoButton);
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(littleButton);
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(considerableButton);
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(mediumButton);
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(muchButton);
+        controls.add(Box.createRigidArea(new Dimension(0, 30)));
+        controls.add(extremeButton);
+
         // reseta a imagem se travar
-        new javax.swing.Timer(16, e -> repaint()).start();
-        // centraliza a pagina
-        setLocationRelativeTo(null);
+        repaintTimer = new Timer(16, e -> imagePanel.repaint());
+        repaintTimer.start();
+        // define um tamanho pra quando a pessoa tirar da tela cheia
+        setSize(1200, 800);
+        // faz abrir em tela cheia
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        SwingUtilities.invokeLater(() -> {
+            BufferedImage img = CameraASCII.ASCIIImage;
+            if (img != null) {
+                split.setDividerLocation(img.getWidth());
+            }
+        });
+
         setVisible(true);
     }
 }
 class ImagePanel extends JPanel {
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
+    public Dimension getPreferredSize() {
         BufferedImage img = CameraASCII.ASCIIImage;
         if (img != null) {
-            g.drawImage(img, 0, 0, this);
+            return new Dimension(img.getWidth(), img.getHeight());
+        }
+        return new Dimension(800, 600); // fallback
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        BufferedImage img = CameraASCII.ASCIIImage;
+        if (img != null) {
+            g.drawImage(img, 0, 0, null);
         }
     }
 }
@@ -131,7 +276,6 @@ class ConvertGrayThread extends Thread {
                 height,
                 BufferedImage.TYPE_BYTE_GRAY
         );
-
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
 
@@ -152,16 +296,17 @@ class ConvertGrayThread extends Thread {
     }
 }
 class ConvertImageToASCII extends Thread {
-
-    static String littleLighting = " .=10?%#$&@";
-    static String mediumLighting = "  .=10?#@";
-    static String muchLighting = "   .=10?#";
     @Override
     public void run() {
         while (CameraASCII.running) {
             BufferedImage image = CameraASCII.grayImage;
             if (image != null) {
-                CameraASCII.ASCIIImage = convertGrayToASCII(image,190,90);
+                CameraASCII.ASCIIImage = convertGrayToASCII(image,158,80);
+                SwingUtilities.invokeLater(() -> {
+                        Window.imagePanel.revalidate();
+                    Window.imagePanel.repaint();
+                    Window.split.setDividerLocation(CameraASCII.ASCIIImage.getWidth());
+                });
             }
             try {
                 Thread.sleep(16);
@@ -174,7 +319,7 @@ class ConvertImageToASCII extends Thread {
     public static BufferedImage convertGrayToASCII(BufferedImage grayImage, int cols, int rows) {
 
         // sequência ASCII do mais escuro para o mais claro
-        final String ASCII = mediumLighting; // <------- Caracteres
+        final String ASCII = CameraASCII.characterValue; // <------- Caracteres
 
         // largura da imagem de entrada
         int imgW = grayImage.getWidth();
